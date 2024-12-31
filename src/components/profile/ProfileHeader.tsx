82091eb205f63a4f5d42a93abaa91377da920e71
Alastair7 <alexandru.ban.drmer@gmail.com>
2024-12-31 02:04:24 +0100

    add floating animation for social media icons and add social media icons

--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -1,3 +1,5 @@
+import GithubIcon from "../../assets/icons/github.svg";
+import LinkedinIcon from "../../assets/icons/linkedin.svg";
 import Avatar from "../../assets/images/avatar.png";
 import { useWorkExperience } from "../../shared/hooks/useWorkExperience";
 
@@ -6,8 +8,10 @@ export const ProfileHeader = () => {
 
   return (
     <>
-      <div className="flex justify-center">
+      <div className="flex justify-center items-center gap-5">
+        <LinkedinIcon className="size-10 animate-floating" />
         <img src={Avatar} className="size-24 mt-2" />
+        <GithubIcon className="size-10 animate-floating" />
       </div>
 
       <div className="flex flex-col items-center -space-y-1">
@@ -20,7 +24,7 @@ export const ProfileHeader = () => {
       <div className="p-3 text-center text-orange-300">
         <p>
           I'm a Software Engineer in love with the process of learning and
-          crafting software..
+          crafting software.
         </p>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        floating: {
+          "0%": { transform: "translatey(0px)" },
+          "25%": { transform: "translatey(3px)" },
+          "50%": { transform: "translatey(5px)" },
+          "75%": { transform: "translatey(2px)" },
+          "100%": { transform: "translatey(0px)" },
+        },
+      },
+      animation: {
+        floating: "floating 3.3s linear infinite",
+      },
+    },
   },
   plugins: [],
 };

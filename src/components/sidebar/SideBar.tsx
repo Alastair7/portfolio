3dff18b2fde709bc226b3a9a5b2ec9ea741fbc3a
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-05-24 01:01:42 +0200

    Add styling for sidebar

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,5 +1,6 @@
 import './SideBar.scss';
 import GithubIcon from '../../assets/icons/github.svg?react';
+import LinkednIcon from '../../assets/icons/linkedn.svg?react';
 import { LinkButton } from '../../shared/buttons/LinkButton';
 
 export const SideBar = () => {
@@ -10,8 +11,18 @@ export const SideBar = () => {
         <h2>Software Engineer</h2>
       </div>
       <div className="header__social">
-        <LinkButton href="https://github.com/Alastair7" icon={GithubIcon} title="@Alastair7" />
-        <LinkButton icon={GithubIcon} title="Alexandru Ban" />
+        <LinkButton
+          href="https://github.com/Alastair7"
+          target="_blank"
+          icon={GithubIcon}
+          title="@Alastair7"
+        />
+        <LinkButton
+          href="https://linkedin.com/in/alexandru-ban"
+          target="_blank"
+          icon={LinkednIcon}
+          title="Alexandru Ban"
+        />
       </div>
     </header>
   );

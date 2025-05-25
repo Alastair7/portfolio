058b40f2226fe7b0cdc28a496a2530deddaa5c93
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-05-25 21:09:12 +0200

    Add months to my experience info

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -3,27 +3,30 @@ import './About.scss';
 import { Terminal, type TerminalCommand } from '../terminal/Terminal';
 
 export const About = () => {
-  const [experience, setExperience] = useState<number>(0);
+  const [experience, setExperience] = useState<string>('');
   const aboutInfo: Map<TerminalCommand, string> = new Map();
 
   useEffect(() => {
     const startDate = new Date(2021, 0, 11);
     const currentTime = new Date();
-    let currentExperience = Math.abs(startDate.getFullYear() - currentTime.getFullYear());
+
+    let currentExperienceYears = Math.abs(startDate.getFullYear() - currentTime.getFullYear());
+
+    let currentExperienceMonths = Math.abs(startDate.getMonth() - currentTime.getMonth());
     const anniversary = new Date(currentTime.getFullYear(), 0, 11);
 
     if (currentTime < anniversary) {
-      currentExperience -= 1;
+      currentExperienceYears -= 1;
     }
 
-    setExperience(currentExperience);
+    setExperience(`${currentExperienceYears} years ${currentExperienceMonths} months`);
   }, []);
 
   aboutInfo.set(
     'info',
     `Fullstack engineer that likes (a lot) crafting software. I tend to build side-projects about things that motivates me. For example, I had this idea of making a portfolio with my own style, so... here it is :)`,
   );
-  aboutInfo.set('experience', `${experience.toString()} years`);
+  aboutInfo.set('experience', experience);
   aboutInfo.set('editor', 'I use Neovim btw');
   return <Terminal content={aboutInfo} />;
 };

15af38034117e9b5d87927000d7a3ade6d51cd70
Alastair7 <alexandru.ban.drmer@gmail.com>
2026-07-09 23:39:20 +0200

    Rewrite and improve styling of the about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter } from 'react-router'
-import './App.css'
 import { AppRoutes } from './shared/routing/AppRoutes'
 
 function App() {
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,40 +4,83 @@ import "./About.css"
 export const AboutPage = () => {
   const age = 2026 - 1998
   return <>
-    <h1>Alexandru Ban</h1>
-    <h2>About me</h2>
-    <Menu />
+    <header className="header">
+      <h1 className="header__title">Alexandru Ban</h1>
+      <h2 className="header__subtitle">Fullstack Software Engineer</h2>
+      <Menu />
+    </header>
 
     <article className="about">
-      <h1 className="about__title">More details about me</h1>
+      <h1 className="about__title">Who am I?</h1>
       <h2 className="about__section">The Professional</h2>
-      <p className="about__text">I am a software engineer currently working at Plain Concepts for already 2 years.
-        I have been working for 4 years in the software development world; Previously in ViewNext
-        for 2 years.
-      </p>
+      <div className="about__textarea">
 
-      <p className="about__text">I am specialized in backend development but I have also experience in frontend.
-        To be honest, I would always be a backend guy, because it is what I like the most.</p>
-      <p className="about__text">Since AI has came to our lives, I have also experience with AI projects such as: RAG systems,
-        conversational bots, AI focused systems...</p>
+        <p className="about__text">
+          I am a software engineer with 4 years of experience in software development.
+          My primary specialization is backend development, but I am also comfortable working on the frontend.
+        </p>
 
-      <h2 className="about__section">The Dream Chaser</h2>
-      <p className="about__text">If my professional presentation is not enough and you want to know more about me. Well,
-        I am a {age} years old guy who likes programming.
-      </p>
-      <p className="about__text">
-        Since high school I knew I wanted to be a programmer and the thought of "materializing" an idea really clicked with me.
-      </p>
-      <p className="about__text">That is how I see programming. I can create anything I want, I feel free.</p>
+        <br />
 
-      <h2 className="about__section">The Kid</h2>
-      <p className="about__text">We all have been kids. Me too, I promise.</p>
-      <p className="about__text">As a little boy, I was not interested in programming, even though I grew up with a computer.</p>
-      <p className="about__text">That is why I have another hobby such as videogames. I enjoy playing videogames, and the game I enjoyed the most is the Mass Effect trilogy. Such a ride.
-      </p>
-      <p className="about__text">I also try to have a healthy life by doing some exercise and doing at least 7000-10000 steps each day.</p>
-      <p className="about__text">But well, I am not the most athletic guy, but I am trying my best.</p>
-    </article>
+        <p className="about__text">
+          I have experience designing distributed systems and have contributed to the development of projects built on distributed architectures. My experience also includes designing and implementing RESTful APIs and building responsive SPAs and PWAs with a strong focus on performance, scalability, and maintainability.
+        </p>
+        <br />
+
+        <p className="about__text">
+          Furthermore, I have hands-on experience with Infrastructure as Code (IaC) using Terraform and extensive experience working with Microsoft Azure.
+        </p>
+        <br />
+
+        <p className="about__text">
+          I have also contributed to AI-related projects, including the development of Retrieval-Augmented Generation (RAG) systems, conversational agents, and evaluation processes for RAG-based applications.
+        </p>
+
+        <h2 className="about__section">The Dream Chaser</h2>
+        <p className="about__text">
+          Enough with the corporative / professional language. I am a {age} years old guy who likes programming.
+        </p>
+        <p className="about__text">
+          Since high school I knew I wanted to be a programmer and the thought of "materializing" an idea really clicked with me.
+        </p>
+        <br />
+        <p className="about__text">
+          This section is called the dream chaser because most of my life I have been working hard to achieve this dream.
+          Spent many days and nights learning, studying and practicing to improve my skills, and even know I keep doing it.
+        </p>
+        <br />
+        <p className="about__text">
+          I have also some personal projects going on such as: GGTime, ArchyByte and this portfolio. And of course, a lot of unfinished projects in my github. But, everything that I have done was a worth lesson learned.
+        </p>
+
+        <h2 className="about__section">The Person</h2>
+        <p className="about__text">
+          I like programming, but I also enjoy different hobbies other than reading algorithms and solving problems.
+        </p>
+        <br />
+
+        <p className="about__text">
+          I grew up playing videogames, I really like them and they always have been part of my life.
+          I do not have a specific favorite videogame but one that I liked the most is probably the Mass Effect trilogy.
+        </p>
+        <br />
+
+        <p className="about__text">
+          I also enjoy reading books, my two favorites series are <cite>Stormlight Archive</cite> by Brandon Sanderson and <cite>Ender's Game</cite> by Orson Scott Card. I normally read sci-fi or fantasy when I had enough of reading software engineering books.
+        </p>
+        <br />
+
+
+        <p className="about__text">
+          I also try to keep a healthy life and keep my body in movement, because my work and hobbies are not really about touching grass. That is why I put to myself this daily goal to at least go out and do minimum 7000 steps, and also there are days where I like to go running, because when I was in high school I used to run a lot.
+        </p>
+        <br />
+
+        <p className="about__text">
+          All of these hobbies are great, but the thing that I enjoy the most is spending quality time with my beloved person, Paula ❤️. I had to mention her, because she is also part of my life and she also is helping me with the portfolio and my blog.
+        </p>
+      </div>
+    </article >
   </>
 }
 
